41633d204bb7f3a42f992308f90062890c7f9ef8
doyeon0307 <eg4rang@naver.com>
2024-09-19 16:28:51 +0900

    webview

--- url_launcher_and_webview/lib/main.dart
+++ url_launcher_and_webview/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher_and_webview/webview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,73 +36,83 @@ class MyHomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                final Uri webLaunchUrl = Uri(
-                  scheme: "https",
-                  path: "naver.com",
-                );
-                try {
-                  launchUrl(webLaunchUrl);
-                } catch (e) {
-                  Exception(e);
-                }
-              },
-              child: const Text("웹 브라우저"),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final Uri emailLaunchUri = Uri(
-                  scheme: "mailto",
-                  path: "dodo03@khu.ac.kr",
-                  query: encodeQueryParameters(<String, String>{
-                    "subject": "제목 입력",
-                    "body": "본문 입력",
-                  }),
-                );
-                try {
-                  launchUrl(emailLaunchUri);
-                } catch (e) {
-                  Exception(e);
-                }
-              },
-              child: const Text("메일"),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final Uri smsLaunchUri = Uri(
-                  scheme: "sms",
-                  path: "010 1122 3344",
-                  query: encodeQueryParameters(<String, String>{
-                    "body": "본문 입력",
-                  }),
-                );
-                try {
-                  launchUrl(smsLaunchUri);
-                } catch (e) {
-                  Exception(e);
-                }
-              },
-              child: const Text("문자"),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final Uri telLaunchUri = Uri(
-                  scheme: "tel",
-                  path: "010 1122 3344",
-                );
-                try {
-                  launchUrl(telLaunchUri);
-                } catch (e) {
-                  Exception(e);
-                }
-              },
-              child: const Text("전화"),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  final Uri webLaunchUrl = Uri(
+                    scheme: "https",
+                    path: "naver.com",
+                  );
+                  try {
+                    launchUrl(webLaunchUrl);
+                  } catch (e) {
+                    Exception(e);
+                  }
+                },
+                child: const Text("웹 브라우저"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final Uri emailLaunchUri = Uri(
+                    scheme: "mailto",
+                    path: "dodo03@khu.ac.kr",
+                    query: encodeQueryParameters(<String, String>{
+                      "subject": "제목 입력",
+                      "body": "본문 입력",
+                    }),
+                  );
+                  try {
+                    launchUrl(emailLaunchUri);
+                  } catch (e) {
+                    Exception(e);
+                  }
+                },
+                child: const Text("메일"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final Uri smsLaunchUri = Uri(
+                    scheme: "sms",
+                    path: "010 1122 3344",
+                    query: encodeQueryParameters(<String, String>{
+                      "body": "본문 입력",
+                    }),
+                  );
+                  try {
+                    launchUrl(smsLaunchUri);
+                  } catch (e) {
+                    Exception(e);
+                  }
+                },
+                child: const Text("문자"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final Uri telLaunchUri = Uri(
+                    scheme: "tel",
+                    path: "010 1122 3344",
+                  );
+                  try {
+                    launchUrl(telLaunchUri);
+                  } catch (e) {
+                    Exception(e);
+                  }
+                },
+                child: const Text("전화"),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => WebviewScreen(),
+                  ),
+                ),
+                child: const Text("웹뷰로 이동"),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ url_launcher_and_webview/lib/webview_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebviewScreen extends StatefulWidget {
+  const WebviewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WebviewScreen> createState() => _WebviewScreenState();
+}
+
+class _WebviewScreenState extends State<WebviewScreen> {
+  late final WebViewController controller;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            setState(() {
+              isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Webview"),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: controller),
+          if (isLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
+    );
+  }
+}
